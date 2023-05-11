710be1774789f0ea41fc8df0383aa445237af8fa
Karam47 <77400199+william94411@users.noreply.github.com>
2023-05-11 03:47:57 +0300

    .
    .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://ony8olnxr8.execute-api.us-east-1.amazonaws.com/',
+  cognito: {
+    userPoolId: 'us-east-1_ccEjEcAFc',
+    userPoolWebClientId: '1b798gjip7h81aa5q22e14dh7o',
+  }
+};
